11bea50069ce80e56f109f34e197f44b6c2c0686
helox <eyes22798@gmail.com>
2024-04-18 16:07:24 +0800

    chore: :hammer: add cz-git

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -9,24 +9,28 @@ module.exports = {
 			['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'revert', 'chore'],
 		],
 	},
-	types: [
-		{ value: 'feat', emoji: ':sparkles:' },
-		{ value: 'fix', emoji: ':bug:' },
-		{ value: 'docs', emoji: ':memo:' },
-		{ value: 'style', emoji: ':lipstick:' },
-		{ value: 'refactor', emoji: ':recycle:' },
-		{ value: 'perf', emoji: ':zap:' },
-		{ value: 'test', emoji: ':white_check_mark:' },
-		{
-			value: 'build',
-			emoji: ':package:',
-		},
-		{ value: 'ci', emoji: ':ferris_wheel:' },
-		{ value: 'revert', emoji: ':rewind:' },
-		{
-			value: 'chore',
-			emoji: ':hammer:',
-		},
-	],
-	useEmoji: true,
+	prompt: {
+		types: [
+			{ name: 'feat', value: 'feat', emoji: ':sparkles:' },
+			{ name: 'fix', value: 'fix', emoji: ':bug:' },
+			{ name: 'docs', value: 'docs', emoji: ':memo:' },
+			{ name: 'style', value: 'style', emoji: ':lipstick:' },
+			{ name: 'refactor', value: 'refactor', emoji: ':recycle:' },
+			{ name: 'perf', value: 'perf', emoji: ':zap:' },
+			{ name: 'test', value: 'test', emoji: ':white_check_mark:' },
+			{
+				name: 'build',
+				value: 'build',
+				emoji: ':package:',
+			},
+			{ name: 'ci', value: 'ci', emoji: ':ferris_wheel:' },
+			{ name: 'revert', value: 'revert', emoji: ':rewind:' },
+			{
+				name: 'chore',
+				value: 'chore',
+				emoji: ':hammer:',
+			},
+		],
+		useEmoji: true,
+	},
 }
