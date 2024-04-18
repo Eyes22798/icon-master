44ea1178c47a30593df0af73b54fe2f84a949621
helox <eyes22798@gmail.com>
2024-04-18 15:01:18 +0800

    chore: add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,28 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
 import parserVue from 'vue-eslint-parser'
-
+import pluginPrettierRecommendedConfigs from 'eslint-plugin-prettier/recommended'
 
 export default [
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.es2020,
-        ...globals.node,
-      },
-      ecmaVersion: 2020,
-      parser: parserVue,
-      parserOptions: {
-        parser: tseslint.parser,
-      },
-    },
-    ignores: ["packages/*/dist", "packages/*/lib", "packages/*/html", "*/**.d.ts", "*/**.js"],
-  },
-];
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	...pluginVue.configs['flat/essential'],
+	pluginPrettierRecommendedConfigs,
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.es2020,
+				...globals.node,
+			},
+			ecmaVersion: 2020,
+			parser: parserVue,
+			parserOptions: {
+				parser: tseslint.parser,
+			},
+		},
+		ignores: ['packages/*/dist', 'packages/*/lib', 'packages/*/html', '*/**.d.ts', '*/**.js'],
+	},
+]
